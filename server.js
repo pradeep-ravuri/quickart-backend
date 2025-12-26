7aa8a2313ecbfbe69a061b2d45318fcacc69a141
Pradeep Ravuri <pradeep_ravuri@srmap.edu.in>
2025-12-27 02:58:01 +0530

    Fix API routes and OTP routing

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ import userRouter from "./routes/userRoute.js";
 import productRouter from "./routes/productRoute.js";
 import cartRouter from "./routes/cartRoute.js";
 import orderRouter from "./routes/orderRoute.js";
-import otpRouter from "./routes/otpRoute.js";
 
 //App config
 const app = express();
@@ -23,8 +22,6 @@ app.use("/api/user", userRouter);
 app.use("/api/product", productRouter);
 app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
-
-app.use("/api/otp", otpRouter);
 
 app.get("/", (req, res) => {
   res.send("API working");
